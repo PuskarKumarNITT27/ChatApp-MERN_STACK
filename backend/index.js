fa40b5d7403baa7555434eee0105d792a76064d6
Puskar Kumar <puskarraj1001@gmail.com>
2026-05-12 19:03:45 +0530

    feature : backend user authentication done

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+import { connectDB } from "./config/db.js"
+import userRoutes from "./routes/user.routes.js"
+import dotenv from "dotenv"
+dotenv.config()
+
+
+connectDB()
+
+const app = express()
+const PORT = process.env.PORT || 5000;
+
+// middlewares
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
+
+// API ENDPOINTS    
+app.get("/",(req,res) =>{
+    res.send("hello from server");
+})
+
+app.use("/api/user",userRoutes);
+
+
+app.listen(PORT,()=>{
+    console.log()
+})
